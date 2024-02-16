d0c57c9ed2fabc1ebc699e7c953a88232691fa5e
95tuanle <tlna95@icloud.com>
2024-02-15 19:51:48 -0500

    style sign in form

--- app/(overview)/sign-in/page.tsx
+++ app/(overview)/sign-in/page.tsx
@@ -7,40 +7,77 @@ export default function Page() {
   const [errorMessage, dispatch] = useFormState(authenticate, undefined);
 
   return (
-    <form action={dispatch}>
-      <label className="" htmlFor="email">
-        Email
-      </label>
-      <input
-        className="text-black"
-        id="email"
-        type="email"
-        name="email"
-        placeholder="Enter your email address"
-        required
-      />
-      <label className="" htmlFor="password">
-        Password
-      </label>
-      <input
-        className="text-black"
-        id="password"
-        type="password"
-        name="password"
-        placeholder="Enter password"
-        required
-        minLength={8}
-      />
-      <div>{errorMessage && <p>{errorMessage}</p>}</div>
-      <LoginButton />
-    </form>
+    <main className="flex items-center justify-center">
+      <div className="relative mx-auto flex w-full max-w-[400px] flex-col space-y-2.5 p-4">
+        <form action={dispatch} className="space-y-3">
+          <div className="text-black flex-1 rounded-lg bg-gray-50 px-6 pb-4 pt-8">
+            <h1 className="mb-3 text-2xl">Please log in to continue.</h1>
+            <div className="w-full">
+              <div>
+                <label
+                  className="mb-3 mt-5 block text-xs font-medium"
+                  htmlFor="email"
+                >
+                  Email
+                </label>
+                <div className="relative">
+                  <input
+                    className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
+                    id="email"
+                    type="email"
+                    name="email"
+                    placeholder="Enter your email address"
+                    required
+                  />
+                </div>
+              </div>
+              <div>
+                <label
+                  className="mb-3 mt-5 block text-xs font-medium"
+                  htmlFor="password"
+                >
+                  Password
+                </label>
+                <div className="relative">
+                  <input
+                    className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
+                    id="password"
+                    type="password"
+                    name="password"
+                    placeholder="Enter password"
+                    required
+                    minLength={8}
+                  />
+                </div>
+              </div>
+            </div>
+            <LoginButton />
+            <div
+              className="flex h-8 items-end space-x-1"
+              aria-live="polite"
+              aria-atomic="true"
+            >
+              {errorMessage && (
+                <>
+                  <p className="text-sm text-red-500">{errorMessage}</p>
+                </>
+              )}
+            </div>
+          </div>
+        </form>
+      </div>
+    </main>
   );
 }
 
 function LoginButton() {
   const { pending } = useFormStatus();
   return (
-    <button aria-disabled={pending} type="submit">
+    <button
+      className="mt-4 w-full hover:bg-sky-100 text-gray-600"
+      aria-disabled={pending}
+      type="submit"
+    >
       Sign in
     </button>
   );
