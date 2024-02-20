b149d00c0f3f3c65fd89b989ae5d5a9e524c8654
95tuanle <tlna95@icloud.com>
2024-02-20 01:23:29 -0500

    add nav bar for signed in ui

--- app/(overview)/layout.tsx
+++ app/(overview)/layout.tsx
@@ -1,10 +1,18 @@
 import NavBar from '@/app/ui/nav-bar';
 import React from 'react';
 
+const links = [
+  { name: 'JoinIn', href: '/' },
+  { name: 'Sign in', href: '/sign-in' },
+  {
+    name: 'Sign up',
+    href: '/sign-up',
+  },
+];
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <>
-      <NavBar />
+      <NavBar links={links} isSignedIn={false} />
       {children}
     </>
   );
