555da42f3238251098e06f51b18d559756a1c193
Nguyen Anh Tuan Le <tlna95@icloud.com>
2026-05-13 17:32:53 +0700

    Update page.tsx

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -2,22 +2,18 @@ import { Suspense } from 'react';
 import { Loading } from '@/app/ui/loading';
 import UpcomingEvents from '@/app/ui/home/upcoming-events';
 
+export const dynamic = 'force-dynamic';
+
 export default function Page() {
   return (
     <div className="w-full">
       <div className="flex w-full items-center justify-between">
-        <h1 className={`text-2xl`}>Upcoming events</h1>
+        <h1 className="text-2xl">Upcoming events</h1>
       </div>
-      {/*<div className="mt-4 flex items-center justify-between gap-2">*/}
-      {/*  <Search placeholder="Search events..." />*/}
-      {/*  <CreateEvent />*/}
-      {/*</div>*/}
+
       <Suspense fallback={<Loading />}>
         <UpcomingEvents />
       </Suspense>
-      {/*<div className="mt-5 flex w-full justify-center">*/}
-      {/*  <Pagination totalPages={totalPages} />*/}
-      {/*</div>*/}
     </div>
   );
 }
