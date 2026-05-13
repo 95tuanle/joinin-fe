834756aebcf7b37d5a16b419f8be0668c9bd1722
Nguyen Anh Tuan Le <tlna95@icloud.com>
2026-05-13 17:25:07 +0700

    Update page.tsx

--- app/home/event-detail/[_id]/page.tsx
+++ app/home/event-detail/[_id]/page.tsx
@@ -2,12 +2,21 @@ import { Suspense } from 'react';
 import { Loading } from '@/app/ui/loading';
 import EventDetail from '@/app/ui/home/event-detail';
 
-export default function Page({ params: { _id } }: { params: { _id: string } }) {
+type PageProps = {
+  params: Promise<{
+    _id: string;
+  }>;
+};
+
+export default async function Page({ params }: PageProps) {
+  const { _id } = await params;
+
   return (
     <div className="w-full">
       <div className="flex w-full items-center justify-between">
-        <h1 className={`text-2xl`}>Event detail</h1>
+        <h1 className="text-2xl">Event detail</h1>
       </div>
+
       <Suspense fallback={<Loading />}>
         <EventDetail _id={_id} />
       </Suspense>
